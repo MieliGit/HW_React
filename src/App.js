1642375f2a00fff4253861b1358951e196cd1884
Daniil Zakharov <zdan14@mail.ru>
2024-10-01 12:32:00 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Message from "./Components/Message";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__header"></div>
+      <Message text="Hello" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Message.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Message(props) {
+  return <h1 className="app__massage">{props.text}</h1>;
+}
+
+export default Message;
